d36c9eef6a6d27e195178672e6f5908a6a42f5bc
jack <jack@k-net.pro>
2017-07-21 21:42:55 +0200

    tools/fetch_ip_asn.py: support multiprocessing
    Signed-off-by: jack <jack@k-net.pro>

--- tools/fetch_ip_asn.py
+++ tools/fetch_ip_asn.py
@@ -3,26 +3,40 @@
 import sys
 import requests
 import json
+from multiprocessing import Pool
 
-if len(sys.argv) != 2:
-	print('Usage: %s <asinfo.txt>' % (sys.argv[0],))
+url = r'https://stat.ripe.net/data/announced-prefixes/data.json?resource=AS%s'
+
+
+def work(asn):
+	print('Fetching %s ..' % (asn,))
+	try:
+		open('%s.json' % (i,))
+		return
+	except:
+		pass
+
+	data = requests.get(url % (asn,))
+	data = data.json()['data']['prefixes']
+	data = [i['prefix'] for i in data]
+	with open('%s.json' % (asn,), 'w') as f:
+		f.write(json.dumps(data))
+
+
+if len(sys.argv) < 2 or len(sys.argv) > 3:
+	print('Usage: %s <asinfo.txt> [nproc]' % (sys.argv[0],))
 	sys.exit(1)
 
 asn = open(sys.argv[1], newline='').readlines()
 asn = [i.rstrip().split('\t')[0] for i in asn]
 
-for i in asn:
-	print('Fetching %s ..' % (i,))
-	try:
-		open('%s.json' % (i,))
-		continue
-	except:
-		pass
-	data = requests.get('https://stat.ripe.net/data/announced-prefixes/data.json?resource=AS%s' % (i,))
-	data = data.json()['data']['prefixes']
-	data = [i['prefix'] for i in data]
-	with open('%s.json' % (i,), 'w') as f:
-		f.write(json.dumps(data))
+try:
+	nproc = int(sys.argv[2])
+except IndexError:
+	nproc = 4
+
+with Pool(processes=nproc) as pool:
+	pool.map(work, asn)
 
 result = {}
 for i in asn:
